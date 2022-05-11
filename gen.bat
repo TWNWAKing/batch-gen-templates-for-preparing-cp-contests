f5590a97df7113b3ca7d71ebeff26e0c4bab68e1
TWNWAKing <shashashane0427@gmail.com>
2022-05-11 14:43:04 +0800

    add generator

---
+++ gen.bat
@@ -0,0 +1,494 @@
+@echo off
+set /p name= "Enter contest name: "
+mkdir "%name%"
+cd "%name%"
+set /p choice= "Enter the problem character of final problem: " 
+If "%choice%" == "A" Goto VA
+If "%choice%" == "B" Goto VB
+If "%choice%" == "C" Goto VC
+If "%choice%" == "D" Goto VD
+If "%choice%" == "E" Goto VE
+If "%choice%" == "F" Goto VF
+If "%choice%" == "G" Goto VG
+If "%choice%" == "H" Goto VH
+If "%choice%" == "I" Goto VI
+If "%choice%" == "J" Goto VJ
+If "%choice%" == "K" Goto VK
+If "%choice%" == "L" Goto VL
+If "%choice%" == "M" Goto VM
+If "%choice%" == "N" Goto VN
+If "%choice%" == "O" Goto VO
+If "%choice%" == "P" Goto VP
+If "%choice%" == "Q" Goto VQ
+If "%choice%" == "R" Goto VR
+If "%choice%" == "S" Goto VS
+If "%choice%" == "T" Goto VT
+If "%choice%" == "U" Goto VU
+If "%choice%" == "V" Goto VV
+If "%choice%" == "W" Goto VW
+If "%choice%" == "X" Goto VX
+If "%choice%" == "Y" Goto VY
+If "%choice%" == "Z" Goto VZ
+Goto Error
+
+:VA
+type template_directory > A.cpp
+Pause
+Exit
+
+:VB
+type template_directory > A.cpp
+type template_directory > B.cpp
+Pause
+Exit
+
+:VC
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+Pause
+Exit
+
+:VD
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+Pause
+Exit
+
+:VE
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+Pause
+Exit
+
+:VF
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+Pause
+Exit
+
+:VG
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+Pause
+Exit
+
+:VH
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+Pause
+Exit
+
+:VI
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+Pause
+Exit
+
+:VJ
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+type template_directory > J.cpp
+Pause
+Exit
+
+:VK
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+type template_directory > J.cpp
+type template_directory > K.cpp
+Pause
+Exit
+
+:VL
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+type template_directory > J.cpp
+type template_directory > K.cpp
+type template_directory > L.cpp
+Pause
+Exit
+
+:VM
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+type template_directory > J.cpp
+type template_directory > K.cpp
+type template_directory > L.cpp
+type template_directory > M.cpp
+Pause
+Exit
+
+:VN
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+type template_directory > J.cpp
+type template_directory > K.cpp
+type template_directory > L.cpp
+type template_directory > M.cpp
+type template_directory > N.cpp
+Pause
+Exit
+
+:VO
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+type template_directory > J.cpp
+type template_directory > K.cpp
+type template_directory > L.cpp
+type template_directory > M.cpp
+type template_directory > N.cpp
+type template_directory > O.cpp
+Pause
+Exit
+
+:VP
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+type template_directory > J.cpp
+type template_directory > K.cpp
+type template_directory > L.cpp
+type template_directory > M.cpp
+type template_directory > N.cpp
+type template_directory > O.cpp
+type template_directory > P.cpp
+Pause
+Exit
+
+:VQ
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+type template_directory > J.cpp
+type template_directory > K.cpp
+type template_directory > L.cpp
+type template_directory > M.cpp
+type template_directory > N.cpp
+type template_directory > O.cpp
+type template_directory > P.cpp
+type template_directory > Q.cpp
+Pause
+Exit
+
+:VR
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+type template_directory > J.cpp
+type template_directory > K.cpp
+type template_directory > L.cpp
+type template_directory > M.cpp
+type template_directory > N.cpp
+type template_directory > O.cpp
+type template_directory > P.cpp
+type template_directory > Q.cpp
+type template_directory > R.cpp
+Pause
+Exit
+
+:VS
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+type template_directory > J.cpp
+type template_directory > K.cpp
+type template_directory > L.cpp
+type template_directory > M.cpp
+type template_directory > N.cpp
+type template_directory > O.cpp
+type template_directory > P.cpp
+type template_directory > Q.cpp
+type template_directory > R.cpp
+type template_directory > S.cpp
+Pause
+Exit
+
+:VT
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+type template_directory > J.cpp
+type template_directory > K.cpp
+type template_directory > L.cpp
+type template_directory > M.cpp
+type template_directory > N.cpp
+type template_directory > O.cpp
+type template_directory > P.cpp
+type template_directory > Q.cpp
+type template_directory > R.cpp
+type template_directory > S.cpp
+type template_directory > T.cpp
+Pause
+Exit
+
+:VU
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+type template_directory > J.cpp
+type template_directory > K.cpp
+type template_directory > L.cpp
+type template_directory > M.cpp
+type template_directory > N.cpp
+type template_directory > O.cpp
+type template_directory > P.cpp
+type template_directory > Q.cpp
+type template_directory > R.cpp
+type template_directory > S.cpp
+type template_directory > T.cpp
+type template_directory > U.cpp
+Pause
+Exit
+
+:VV
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+type template_directory > J.cpp
+type template_directory > K.cpp
+type template_directory > L.cpp
+type template_directory > M.cpp
+type template_directory > N.cpp
+type template_directory > O.cpp
+type template_directory > P.cpp
+type template_directory > Q.cpp
+type template_directory > R.cpp
+type template_directory > S.cpp
+type template_directory > T.cpp
+type template_directory > U.cpp
+type template_directory > V.cpp
+Pause
+Exit
+
+:VW
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+type template_directory > J.cpp
+type template_directory > K.cpp
+type template_directory > L.cpp
+type template_directory > M.cpp
+type template_directory > N.cpp
+type template_directory > O.cpp
+type template_directory > P.cpp
+type template_directory > Q.cpp
+type template_directory > R.cpp
+type template_directory > S.cpp
+type template_directory > T.cpp
+type template_directory > U.cpp
+type template_directory > V.cpp
+type template_directory > W.cpp
+Pause
+Exit
+
+:VX
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+type template_directory > J.cpp
+type template_directory > K.cpp
+type template_directory > L.cpp
+type template_directory > M.cpp
+type template_directory > N.cpp
+type template_directory > O.cpp
+type template_directory > P.cpp
+type template_directory > Q.cpp
+type template_directory > R.cpp
+type template_directory > S.cpp
+type template_directory > T.cpp
+type template_directory > U.cpp
+type template_directory > V.cpp
+type template_directory > W.cpp
+type template_directory > X.cpp
+Pause
+Exit
+
+:VY
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+type template_directory > J.cpp
+type template_directory > K.cpp
+type template_directory > L.cpp
+type template_directory > M.cpp
+type template_directory > N.cpp
+type template_directory > O.cpp
+type template_directory > P.cpp
+type template_directory > Q.cpp
+type template_directory > R.cpp
+type template_directory > S.cpp
+type template_directory > T.cpp
+type template_directory > U.cpp
+type template_directory > V.cpp
+type template_directory > W.cpp
+type template_directory > X.cpp
+type template_directory > Y.cpp
+Pause
+Exit
+
+:VZ
+type template_directory > A.cpp
+type template_directory > B.cpp
+type template_directory > C.cpp
+type template_directory > D.cpp
+type template_directory > E.cpp
+type template_directory > F.cpp
+type template_directory > G.cpp
+type template_directory > H.cpp
+type template_directory > I.cpp
+type template_directory > J.cpp
+type template_directory > K.cpp
+type template_directory > L.cpp
+type template_directory > M.cpp
+type template_directory > N.cpp
+type template_directory > O.cpp
+type template_directory > P.cpp
+type template_directory > Q.cpp
+type template_directory > R.cpp
+type template_directory > S.cpp
+type template_directory > T.cpp
+type template_directory > U.cpp
+type template_directory > V.cpp
+type template_directory > W.cpp
+type template_directory > X.cpp
+type template_directory > Y.cpp
+type template_directory > Z.cpp
+Pause
+Exit
+
+:Error
+Echo There is an error in your input. Please try again.
+cd ..
+rmdir "%name%"
+Pause
+Exit
